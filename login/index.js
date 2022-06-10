d3ad18e10ab82fb17c5f9ebfd5184d73738829ec
yookik <410453327@qq.com>
2022-06-10 11:12:45 +0800

    first commit

---
+++ login/index.js
@@ -0,0 +1,18 @@
+// const images = document.querySelectorAll("img");
+
+
+
+// const callback = entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             const image = entry.target;
+//             const data_src = image.getAttribute('data-src');
+//             image.setAttribute('src', data_src);
+//             observer.unobserve(image);
+//         }
+//     });
+// };
+// const observer = new IntersectionObserver(callback);
+// images.forEach(image => {
+//     observer.observe(image);
+// });
